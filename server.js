cd8585c119ff1cd5bd2e8a4e876a05abde82a9f1
cauanzin08 <caua.tupinamba.senai@gmail.com>
2025-08-25 11:50:24 -0300

    FInalizado

--- server.js
+++ server.js
@@ -6,55 +6,66 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(serverPort, () => 
-    console.log(`Servidor Disney iniciado em: http://localhost:${serverPort}`),
+app.get("/", (req,res) => {
     console.log("Bem-vindos(as) ao reino mágico das Princesas Disney! ✨👑"),
-)
-
-app.listen(serverPort, () => {
-    console.log(`Servidor Inicializado na URL http://localhost:${serverPort}`);
-})
+});
 
 app.get("/princesas", (req,res) => {
-    res.status(200). json(princesas);
-})
+    res.status.json(princesas);
+});
 
-    app.get('/princesas/:id', (req, res) => {
-  const id = parseInt(req.params,id);
-  const princesa = princesas.find(p => p.id === id);
-  if(princesa){
-    res.status(200).json(princesa);
+app.get('/princesas/:id', (req, res) => {
+  let id = req.params,id;
+    id = parseInt(id);
+  const idPrincesa = princesas.find((p) => p.id === id);
+    
+  if(idPrincesa){
+    res.status(200).json(idPrincesa);
   }else{
-    res.status(404).json({erro: "Nenhuma princesa com este ID"})
+    res.status(404).json({
+        erro: "Nenhuma princesa com este ID"
+    });
   }
 
 });
     
-    app.get('/princesas),/princesas/nome/:nome', (req, res) => {
-  const nome = req.params.nome.toLowerCase();
-  const princesa = princesas.find(p => p.nome.toLowerCase() === nome)
+app.get('/princesas),/princesas/nome/:nome', (req, res) => {
+  let nome = req.params.nome.toLowerCase();
+  const nomePrincesas = princesas.filter((p) => p.nome.toLocaleLowerCase().includes(nome));
 
-  if(princesa){
-    res.status(200).json(princesa);
+  if(nomePrincesas.lenght > 0) {
+    res.status(200).json(nomePrincesas);
   }else{
-    res.status(404).json({erro:"Nenhuma princesa encontrada com este nome"})
+    res.status(404).json({
+        erro:"Nenhuma princesa encontrada com este nome"
+    });
   }
 });
 
     app.get('/princesas/reino/:reino', (req, res) => {
-  const reino = req.params.reino.toLowerCase();
-  const resultado = princesas.filter(p=> p.reino.toLowerCase() === reino)
+  let reino = req.params.reino.toLowerCase();
+  const reinosEncontrados = princesas.filter((p) => p.reino.toLocaleLowerCase().includes(reino));
 
-  if(resultado.length> 0){
-    res.status(200). json(resultado);
+  if(reinosEncontrados.length> 0){
+    res.status(200). json(reinosEncontrados);
   }else{
-    res.status(404).json({erro: "Nenhuma princesa encontrada neste reino"})
+    res.status(404).json({
+        erro: "Nenhuma princesa encontrada neste reino"
+    });
   }
 
 });
 
-app.get('/princesas/ativas/sim', (req, res) => {
-  const resultado = princesas.filter(p => p.ativa === true);
-  res.status(200).json(resultado);
+app.get('/princesas/ativas/nao', (req, res) => {
+  const princesasAtivas = princesas.filter((p) => !p.ativa);
+    
+    if(princesasAtivas.length > 0){
+      res.status(200).json(princesasAtivas);
+    }else{
+        res.status(404).json({
+            erro: "Nenhuma princesa inativa encontrada"
+});
 
+app.listen(serverPort, () => {
+    console.log(`Reino das Princesas ativo em http://localhost:${serverPort}`);
 })
